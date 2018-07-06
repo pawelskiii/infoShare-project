90eae9e697246f8336b79f744b425369293287c9
Marcin Karwacki <kreatywny20@wp.pl>
2018-07-06 20:08:56 +0200

    feature-FZ9BF-20-MK-smooth scroll - only jQuery (without localScroll)

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,3 +1,17 @@
-$.localScroll({
-    duration: 1000,
+// $.localScroll({
+//     duration: 1000,
+// });
+
+$(document).ready(function () {
+
+    $('a[href^="#"]').on('click', function (event) {
+
+        let target = $($(this).attr('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
 });
