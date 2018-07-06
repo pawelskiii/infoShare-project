2d9067a74aa56fb8edeadf3c51383c5652117d46
Marcin Karwacki <kreatywny20@wp.pl>
2018-07-06 22:27:16 +0200

    feature-FZ9BF-20-MK-smooth scroll jQuery code refactoring

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -2,16 +2,22 @@
 //     duration: 1000,
 // });
 
-$(document).ready(function () {
+// $(document).ready(function () {
+//
+//     $('a[href^="#"]').on('click', function (event) {
+//
+//         let target = $($(this).attr('href'));
+//         if (target.length) {
+//             event.preventDefault();
+//             $('html, body').animate({
+//                 scrollTop: target.offset().top
+//             }, 1000);
+//         }
+//     });
+// });
 
-    $('a[href^="#"]').on('click', function (event) {
-
-        let target = $($(this).attr('href'));
-        if (target.length) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-        }
-    });
+$('a').click(function () {
+    $('html').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000);
 });
