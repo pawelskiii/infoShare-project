f9872dba60ecbfd87a259ed2ae4c4e1fc568fd4b
Marcin Karwacki <kreatywny20@wp.pl>
2018-07-22 16:45:33 +0200

    feature/FZ9BF-13.2-MK-Game-random clouds without animation

--- game/js.js
+++ game/js.js
@@ -1,39 +1,78 @@
+const sectionWidth = 200;
+const $map = $('.map');
+const $numberOfObstacle = parseInt($map.css('width')) / 200; // do usunięcia i zamiany na number of sections ??
+const $numberOfSections = parseInt($map.css('width')) / sectionWidth;
+
 // (function () {
 
-    const obstacleWidth = 80;
-    const obstacleMinHeight = 60;
-    const sectionWidth = 200;
-    const randomizer = .4;
-    let mapObjectTable;
+const obstacleWidth = 80;
+const obstacleMinHeight = 60;
+const randomizer = .4;
+let mapObjectTable;
 
-    const $map = $('.map');
-    const $numberOfObstacle = parseInt($map.css('width')) / 100;
+$('body').append($('<div>').addClass('odnosnik'));
 
-    $('body').append($('<div>').addClass('odnosnik'));
-
-    mapObjectTable = Array
-        .from({length: $numberOfObstacle}, (obstacle, index) => {
-            if (index !== 0) {
-                return {
-                    position: index * sectionWidth + Math.floor(Math.random() * (sectionWidth - obstacleWidth)),
-                    height: Math.floor((Math.random() * 2 + 1)) * obstacleMinHeight
-                }
+mapObjectTable = Array
+    .from({length: $numberOfObstacle}, (obstacle, index) => {
+        if (index !== 0) {
+            return {
+                position: index * sectionWidth + Math.floor(Math.random() * (sectionWidth - obstacleWidth)),
+                height: Math.floor((Math.random() * 2 + 1)) * obstacleMinHeight
             }
-        })
-        .filter(obstacle => {
-            return (obstacle !== undefined && Math.random() > randomizer)
-        })
-        .forEach(obstacle => {
-            $map
-                .append($('<div>')
-                    .addClass('obstacle')
-                    .css('margin-left', obstacle.position)
-                    .css('height', obstacle.height)
-                )
-        });
+        }
+    })
+    .filter(obstacle => {
+        return (obstacle !== undefined && Math.random() > randomizer)
+    })
+    .forEach(obstacle => {
+        $map
+            .append($('<div>')
+                .addClass('obstacle')
+                .css('margin-left', obstacle.position)
+                .css('height', obstacle.height)
+            )
+    });
+
 
 
 // })();
+
+
+//***************CLOUDS***************
+
+(function () {
+const cloudMinWidth = 20;
+const cloudRandomizer = .3;
+let mapCloudTable;
+const $sky = $('.sky');
+// $sky.prepend($('<div>').addClass('cloud-extra-small'));
+// $sky.prepend($('<div>').addClass('cloud-small'));
+// $sky.prepend($('<div>').addClass('cloud-medium'));
+// $sky.prepend($('<div>').addClass('cloud-large'));
+// $sky.prepend($('<div>').addClass('cloud-extra-large'));
+
+mapCloudTable = Array
+    .from({length: $numberOfSections}, (cloud, index) => {
+            return {
+                position: index * sectionWidth + Math.floor(Math.random() * sectionWidth),
+                width: Math.ceil(Math.random()*10)*cloudMinWidth,
+            }
+    })
+    .filter(cloud => Math.random() > cloudRandomizer)
+    .forEach(cloud => {
+        $sky
+            .append($('<div>')
+                .addClass('cloud')
+                .css('margin-left', cloud.position)
+                .css('margin-top', Math.ceil(Math.random()*10)*cloudMinWidth)
+                .css('width', cloud.width)
+                .css('height', cloud.width*.4)
+            )
+    });
+
+console.log(mapCloudTable);
+
+})();
 
 
 /*
