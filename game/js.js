e8f0c66dcf9cdeb55ff38d19fadcbd5cc4c73d35
paweskiii <pawel.racki123@gmail.com>
2018-07-24 23:51:20 +0200

    feature/FZ9BF-13.3-PR-PlayerMovement- preliminary movement version

--- game/js.js
+++ game/js.js
@@ -3,38 +3,236 @@ const $map = $('.map');
 const $numberOfObstacle = parseInt($map.css('width')) / 200; // do usunięcia i zamiany na number of sections ??
 const $numberOfSections = parseInt($map.css('width')) / sectionWidth;
 
-// (function () {
 
-const obstacleWidth = 80;
-const obstacleMinHeight = 60;
-const randomizer = .4;
-let mapObjectTable;
+//***************MAP GENERATOR***************
 
-$('body').append($('<div>').addClass('odnosnik'));
+(function () {
 
-mapObjectTable = Array
-    .from({length: $numberOfObstacle}, (obstacle, index) => {
-        if (index !== 0) {
-            return {
-                position: index * sectionWidth + Math.floor(Math.random() * (sectionWidth - obstacleWidth)),
-                height: Math.floor((Math.random() * 2 + 1)) * obstacleMinHeight
+    const obstacleWidth = 80;
+    const obstacleMinHeight = 60;
+    const randomizer = .4;
+    let mapObjectTable;
+
+    $('body').append($('<div>').addClass('odnosnik'));
+
+    mapObjectTable = Array
+        .from({length: $numberOfObstacle}, (obstacle, index) => {
+            if (index !== 0) {
+                return {
+                    position: index * sectionWidth + Math.floor(Math.random() * (sectionWidth - obstacleWidth)),
+                    height: Math.floor((Math.random() * 2 + 1)) * obstacleMinHeight
+                }
             }
+        })
+        .filter(obstacle => {
+            return (obstacle !== undefined && Math.random() > randomizer)
+        })
+        .forEach(obstacle => {
+            $map
+                .append($('<div>')
+                    .addClass('obstacle')
+                    .css('margin-left', obstacle.position)
+                    .css('height', obstacle.height)
+                )
+        });
+
+})();
+
+//***************PLAYER***************
+
+const player = document.querySelector('#player');
+
+let playerPositionX = 0;
+let playerPositionY = 0;
+let playerSpeedX = 0;
+let playerSpeedY = 0;
+let playerAcceleration = 0.0005;
+let keyPressed = '';
+let jumpKeyPressed = '';
+let time = Date.now();
+
+update();
+
+window.addEventListener('keydown', function (event) {
+    if (event.code === 'ArrowRight' || event.code === 'ArrowLeft') {
+        event.preventDefault();
+        keyPressed = event.code;
+    }
+})
+
+window.addEventListener('keyup', function (event) {
+    if (event.code === 'ArrowRight' || event.code === 'ArrowLeft') {
+        event.preventDefault();
+        keyPressed = '';
+    }
+})
+
+window.addEventListener('keydown', function (event) {
+    if (event.code === 'ArrowUp') {
+        event.preventDefault();
+        jumpKeyPressed = event.code;
+    }
+})
+
+window.addEventListener('keyup', function (event) {
+    if (event.code === 'ArrowUp') {
+        event.preventDefault();
+        jumpKeyPressed = 'jumpReleased';
+    }
+})
+
+function update() {
+
+    const dTime = Date.now() - time;
+    time = Date.now();
+
+    playerPositionX += playerSpeedX * dTime;
+    playerPositionY += playerSpeedY * dTime;
+
+
+    switch (keyPressed) {
+
+        case 'ArrowRight':
+            playerSpeedX = Math.min(
+                Math.max(
+                    0,
+                    playerSpeedX + playerAcceleration * dTime
+                ),
+                0.4
+            );
+            break;
+
+        case 'ArrowLeft':
+            playerSpeedX = Math.max(
+                Math.min(
+                    0,
+                    playerSpeedX - playerAcceleration * dTime
+                ),
+                -0.4
+            );
+            break;
+
+        default:
+            playerSpeedX = 0;
+
+    }
+
+    switch (jumpKeyPressed) {
+
+        case 'ArrowUp':
+            playerSpeedY = playerSpeedY + (jumpKeyPressed === 'ArrowUp' ? 1 : -1) * playerAcceleration * dTime;
+            break;
+
+        case 'jumpReleased':
+            playerSpeedY = playerSpeedY - playerAcceleration * dTime;
+            break;
+
+        default:
+            playerSpeedY = 0;
+
+    }
+
+    if (playerPositionY < 0.01) {
+        playerPositionY = 0;
+    }
+
+
+    player.style.left = playerPositionX + 'px';
+    player.style.bottom = playerPositionY + 'px';
+    requestAnimationFrame(update);
+
+
+}
+
+
+// (function() {
+/*const player = document.querySelector('#player');
+
+let playerPosition = 0;
+let time = Date.now();
+let playerSpeed = 0.01;
+let playerAcceleration = 0.0001;
+let keyPressed = '';
+
+update();
+
+
+window.addEventListener('keydown', function (event) {
+    // console.log(event.code);
+    if (event.code === 'ArrowRight') {
+        event.preventDefault();
+        keyPressed = 'ArrowRight';
+    }
+});
+
+window.addEventListener('keyup', function (event) {
+    if (event.code === 'ArrowRight') {
+        event.preventDefault();
+        keyPressed = '';
+    }
+});
+
+/!*
+const movementKeyTable = ['ArrowRight', 'ArrowLeft', 'ArrowUp']
+
+movementKeyTable.forEach((key) => {
+    window.addEventListener('keydown', function (event) {
+        if (event.code === key) {
+            console.log('1', key)
+            event.preventDefault();
+            keyPressed = key;
         }
     })
-    .filter(obstacle => {
-        return (obstacle !== undefined && Math.random() > randomizer)
+    window.addEventListener('keyup', function (event) {
+        if (event.code === key) {
+            console.log('2', key)
+            event.preventDefault();
+            keyPressed = '';
+            console.log('to jest keypressed', keyPressed, key)
+        }
     })
-    .forEach(obstacle => {
-        $map
-            .append($('<div>')
-                .addClass('obstacle')
-                .css('margin-left', obstacle.position)
-                .css('height', obstacle.height)
-            )
-    });
+})
+*!/
+
+function update() {
+    const dTime = Date.now() - time;
+    time = Date.now();
+
+    playerPosition += playerSpeed * dTime;
+
+    switch (keyPressed) {
+        case 'ArrowRight':
+            console.log('lolol');
+            playerSpeed = Math.min(
+                Math.max(
+                    0,
+                    playerSpeed + (keyPressed === 'ArrowRight' ? 1 : -1) * playerAcceleration * dTime),
+                0.3);
+            console.log('playerRightspeed', playerSpeed);
+            break;
+/!*
+
+        case 'ArrowLeft':
+            console.log(':(:(:(');
+            playerSpeed = Math.max(
+                Math.min(
+                    0,
+                    -(playerSpeed + playerAcceleration * dTime)),
+                -0.3);
+            console.log('playerLEFTspeed', playerSpeed);
+            break;
+*!/
+
+    }
+
+    player.style.marginLeft = playerPosition + 'px';
+    requestAnimationFrame(update)
+}*/
+
+// })();
 
 
-const player = document.getElementById('player')
+/*const player = document.getElementById('player')
 
 function Y(id, countPx) {
     player.style.top = player.style.top.substr(0, player.style.top.length - 2) * 1 + countPx + 'px';
@@ -69,13 +267,11 @@ window.addEventListener('keydown', function (event) {
             Y('player', 15);
             break;
     }
-}, false);
-// })();
-
+}, false);*/
 
 //***************CLOUDS***************
 
-(function () {
+/*(function () {
     const cloudMinWidth = 20;
     const cloudRandomizer = .2;
     let mapCloudTable;
@@ -102,44 +298,4 @@ window.addEventListener('keydown', function (event) {
                     .css('z-index', cloud.zIndex)
                 )
         });
-    console.log(mapCloudTable);
-
-})();
-
-
-/*
-(function () {
-
-    const obstacleWidth = 80;
-    const obstacleMinHeight = 60;
-    const mapWidth = 7000;
-    const mapHeight = 350;
-    const numberOfObstacle = mapWidth / 100;
-    const sectionWidth = 200;
-
-    $('#map').css({
-        width: mapWidth,
-        height: mapHeight
-    });
-
-    $('body').append($('<div>').addClass('odnosnik'));
-
-    const mapObjectTable = [];
-    for (let i = 0; i < numberOfObstacle; i++) {
-
-        mapObjectTable.push({
-            position: Math.floor(Math.random() * obstacleWidth),
-            height: Math.floor((Math.random() * 2 + 1)) * obstacleMinHeight
-        });
-
-        $('#map').append($('<div>')
-            .addClass('obstacle')
-            .css('margin-left', i * sectionWidth + mapObjectTable[i].position)
-            .css('height', mapObjectTable[i].height));
-
-        $('.odnosnik').append($('<div>').addClass('linia'))
-
-    }
-
-})();
-*/
+})()*/
