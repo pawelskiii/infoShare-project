f23b941ec0447bc1e6d9f8af9fbf1d64916daa5d
paweskiii <pawel.racki123@gmail.com>
2018-07-25 18:34:10 +0200

    feature/FZ9BF-13.3-PR-PlayerMovement-minor refactor

--- game/js.js
+++ game/js.js
@@ -39,236 +39,111 @@ const $numberOfSections = parseInt($map.css('width')) / sectionWidth;
 
 //***************PLAYER***************
 
-const player = document.querySelector('#player');
+(function () {
 
-let playerPositionX = 0;
-let playerPositionY = 0;
-let playerSpeedX = 0;
-let playerSpeedY = 0;
-let playerAcceleration = 0.0005;
-let keyPressed = '';
-let jumpKeyPressed = '';
-let time = Date.now();
+    const player = document.querySelector('#player');
 
-update();
+    let playerPositionX = 0;
+    let playerPositionY = 0;
+    let playerSpeedX = 0;
+    let playerSpeedY = 0;
+    let playerAcceleration = 0.0005;
+    let keyPressed = '';
+    let jumpKeyPressed = '';
+    let time = Date.now();
 
-window.addEventListener('keydown', function (event) {
-    if (event.code === 'ArrowRight' || event.code === 'ArrowLeft') {
-        event.preventDefault();
-        keyPressed = event.code;
-    }
-})
+    update();
 
-window.addEventListener('keyup', function (event) {
-    if (event.code === 'ArrowRight' || event.code === 'ArrowLeft') {
-        event.preventDefault();
-        keyPressed = '';
-    }
-})
-
-window.addEventListener('keydown', function (event) {
-    if (event.code === 'ArrowUp') {
-        event.preventDefault();
-        jumpKeyPressed = event.code;
-    }
-})
-
-window.addEventListener('keyup', function (event) {
-    if (event.code === 'ArrowUp') {
-        event.preventDefault();
-        jumpKeyPressed = 'jumpReleased';
-    }
-})
-
-function update() {
-
-    const dTime = Date.now() - time;
-    time = Date.now();
-
-    playerPositionX += playerSpeedX * dTime;
-    playerPositionY += playerSpeedY * dTime;
-
-
-    switch (keyPressed) {
-
-        case 'ArrowRight':
-            playerSpeedX = Math.min(
-                Math.max(
-                    0,
-                    playerSpeedX + playerAcceleration * dTime
-                ),
-                0.4
-            );
-            break;
-
-        case 'ArrowLeft':
-            playerSpeedX = Math.max(
-                Math.min(
-                    0,
-                    playerSpeedX - playerAcceleration * dTime
-                ),
-                -0.4
-            );
-            break;
-
-        default:
-            playerSpeedX = 0;
-
-    }
-
-    switch (jumpKeyPressed) {
-
-        case 'ArrowUp':
-            playerSpeedY = (playerPositionY >= 0) ? playerSpeedY + playerAcceleration * dTime : playerPositionY = 0;
-            break;
-
-        case 'jumpReleased':
-            playerSpeedY = (playerPositionY > 0) ? playerSpeedY - playerAcceleration * dTime : playerPositionY = 0;
-            break;
-
-        default:
-            playerSpeedY = 0;
-
-    }
-
-
-
-
-    player.style.left = playerPositionX + 'px';
-    player.style.bottom = playerPositionY + 'px';
-    requestAnimationFrame(update);
-
-
-}
-
-
-// (function() {
-/*const player = document.querySelector('#player');
-
-let playerPosition = 0;
-let time = Date.now();
-let playerSpeed = 0.01;
-let playerAcceleration = 0.0001;
-let keyPressed = '';
-
-update();
-
-
-window.addEventListener('keydown', function (event) {
-    // console.log(event.code);
-    if (event.code === 'ArrowRight') {
-        event.preventDefault();
-        keyPressed = 'ArrowRight';
-    }
-});
-
-window.addEventListener('keyup', function (event) {
-    if (event.code === 'ArrowRight') {
-        event.preventDefault();
-        keyPressed = '';
-    }
-});
-
-/!*
-const movementKeyTable = ['ArrowRight', 'ArrowLeft', 'ArrowUp']
-
-movementKeyTable.forEach((key) => {
     window.addEventListener('keydown', function (event) {
-        if (event.code === key) {
-            console.log('1', key)
+        if (event.code === 'ArrowRight' || event.code === 'ArrowLeft') {
             event.preventDefault();
-            keyPressed = key;
+            keyPressed = event.code;
         }
-    })
+    });
+
     window.addEventListener('keyup', function (event) {
-        if (event.code === key) {
-            console.log('2', key)
+        if (event.code === 'ArrowRight' || event.code === 'ArrowLeft') {
             event.preventDefault();
             keyPressed = '';
-            console.log('to jest keypressed', keyPressed, key)
         }
-    })
-})
-*!/
+    });
 
-function update() {
-    const dTime = Date.now() - time;
-    time = Date.now();
+    window.addEventListener('keydown', function (event) {
+        if (event.code === 'ArrowUp') {
+            event.preventDefault();
+            jumpKeyPressed = event.code;
+        }
+    });
 
-    playerPosition += playerSpeed * dTime;
+    window.addEventListener('keyup', function (event) {
+        if (event.code === 'ArrowUp') {
+            event.preventDefault();
+            jumpKeyPressed = 'jumpReleased';
+        }
+    });
 
-    switch (keyPressed) {
-        case 'ArrowRight':
-            console.log('lolol');
-            playerSpeed = Math.min(
-                Math.max(
-                    0,
-                    playerSpeed + (keyPressed === 'ArrowRight' ? 1 : -1) * playerAcceleration * dTime),
-                0.3);
-            console.log('playerRightspeed', playerSpeed);
-            break;
-/!*
+    function update() {
 
-        case 'ArrowLeft':
-            console.log(':(:(:(');
-            playerSpeed = Math.max(
-                Math.min(
-                    0,
-                    -(playerSpeed + playerAcceleration * dTime)),
-                -0.3);
-            console.log('playerLEFTspeed', playerSpeed);
-            break;
-*!/
+        const dTime = Date.now() - time;
+        time = Date.now();
+
+        playerPositionX += playerSpeedX * dTime;
+        playerPositionY += playerSpeedY * dTime;
+
+
+        switch (keyPressed) {
+
+            case 'ArrowRight':
+                playerSpeedX = Math.min(
+                    Math.max(
+                        0,
+                        playerSpeedX + playerAcceleration * dTime
+                    ),
+                    0.4
+                );
+                break;
+
+            case 'ArrowLeft':
+                playerSpeedX = Math.max(
+                    Math.min(
+                        0,
+                        playerSpeedX - playerAcceleration * dTime
+                    ),
+                    -0.4
+                );
+                break;
+
+            default:
+                playerSpeedX = 0;
+
+        }
+
+        switch (jumpKeyPressed) {
+
+            case 'ArrowUp':
+                playerSpeedY = (playerPositionY >= 0) ? playerSpeedY + playerAcceleration * dTime : playerPositionY = 0;
+                break;
+
+            case 'jumpReleased':
+                playerSpeedY = (playerPositionY > 0) ? playerSpeedY - playerAcceleration * dTime : playerPositionY = 0;
+                break;
+
+            default:
+                playerSpeedY = 0;
+
+        }
+
+        player.style.left = playerPositionX + 'px';
+        player.style.bottom = playerPositionY + 'px';
+        requestAnimationFrame(update);
 
     }
+})();
 
-    player.style.marginLeft = playerPosition + 'px';
-    requestAnimationFrame(update)
-}*/
-
-// })();
-
-
-/*const player = document.getElementById('player')
-
-function Y(id, countPx) {
-    player.style.top = player.style.top.substr(0, player.style.top.length - 2) * 1 + countPx + 'px';
-
-}
-
-function X(id, countPX) {
-    player.style.left = player.style.left.substr(0, player.style.left.length - 2) * 1 + countPX + 'px';
-}
-
-
-window.addEventListener('keydown', function (event) {
-
-    switch (event.keyCode) {
-        case 37: // Left
-            event.preventDefault();
-            X('player', -15);
-            break;
-
-        case 38: // Up
-            event.preventDefault();
-            Y('player', -15);
-            break;
-
-        case 39: // Right
-            event.preventDefault();
-            X('player', 15);
-            break;
-
-        case 40://Down
-            event.preventDefault();
-            Y('player', 15);
-            break;
-    }
-}, false);*/
 
 //***************CLOUDS***************
 
-/*(function () {
+(function () {
     const cloudMinWidth = 20;
     const cloudRandomizer = .2;
     let mapCloudTable;
@@ -295,4 +170,4 @@ window.addEventListener('keydown', function (event) {
                     .css('z-index', cloud.zIndex)
                 )
         });
-})()*/
+})();
